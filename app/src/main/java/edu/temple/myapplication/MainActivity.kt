ea5218be05d4d3c2463a8fe4fac06d88090152b2
Timothy <timkhumpan@gmail.com>
2024-11-14 14:44:10 -0500

    create service connection and binder

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,67 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+    val timerHandler = Handler(Looper.getMainLooper()) {
+        timerTextView.text = it.what.toString()
+        true
+    }
+    lateinit var timerTextView : TextView
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            timerBinder = p1 as TimerService.TimerBinder
+            isConnected = true
+            timerBinder.setHandler(timerHandler)
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timerTextView = findViewById(R.id.textView)
+
+        bindService(
+            Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE
+        )
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) {
+                timerBinder.start(100)
+            }
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
 
+            if (isConnected) {
+                timerBinder.pause()
+            }
+
         }
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
